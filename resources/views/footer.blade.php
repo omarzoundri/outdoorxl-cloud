307ba1267ea1b90e2e1c0e05aea0e156508c621e
Omar <info@zoundri.nl>
2015-10-16 13:58:46 +0200

    no message

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,17 @@
+<!-- REQUIRED JS SCRIPTS -->
+
+<!-- jQuery 2.1.3 -->
+<script src="{{ asset ("/docs/plugins/jQuery/jQuery-2.1.3.min.js") }}"></script>
+<!-- Bootstrap 3.3.2 JS -->
+<script src="{{ asset ("/docs/bootstrap/js/bootstrap.min.js") }}" type="text/javascript"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset ("/docs/dist/js/app.min.js") }}" type="text/javascript"></script>
+
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright © 2015 <a href="#">Company</a>.</strong> All rights reserved.
+</footer>
