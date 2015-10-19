04db18f6ae26103798a46b71df2e032b6e2183be
Omar <info@zoundri.nl>
2015-10-19 15:50:20 +0200

    added master and add page

---
+++ resources/views/master.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title') OutdoorXL</title>
+    @include('head')
+</head>
+
+<body class="skin-blue">
+<div class="wrapper">
+
+    <!-- Header -->
+    @include('header')
+
+    <!-- Sidebar -->
+    @include('sidebar')
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                {{ $page_title or "Page Title" }}
+                <small>{{ $page_description or null }}</small>
+            </h1>
+            <!-- You can dynamically generate breadcrumbs here -->
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+                <li class="active">Here</li>
+            </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <!-- Your Page Content Here -->
+            @yield('content')
+        </section><!-- /.content -->
+    </div><!-- /.content-wrapper -->
+
+    <!-- Footer -->
+    @include('footer')
+
+</div><!-- ./wrapper -->
+
+<!-- REQUIRED JS SCRIPTS -->
+
+<!-- jQuery 2.1.3 -->
+<script src="{{ asset ("/docs/plugins/jQuery/jQuery-2.1.3.min.js") }}"></script>
+<!-- Bootstrap 3.3.2 JS -->
+<script src="{{ asset ("/docs/bootstrap/js/bootstrap.min.js") }}" type="text/javascript"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset ("/docs/dist/js/app.min.js") }}" type="text/javascript"></script>
+
+<!-- Optionally, you can add Slimscroll and FastClick plugins.
+      Both of these plugins are recommended to enhance the
+      user experience -->
+</body>
+</html>
