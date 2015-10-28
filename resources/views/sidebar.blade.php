2464234719ec82859c45b49801ec8f4f0642f982
Omar <info@zoundri.nl>
2015-10-28 13:04:13 +0100

    added sidebar nav for the add page and employees page.

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -29,17 +29,10 @@
 
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
-            <li class="header">HEADER</li>
+            <li class="header">Beheer</li>
             <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="#"><span>Link</span></a></li>
-            <li><a href="#"><span>Another Link</span></a></li>
-            <li class="treeview">
-                <a href="#"><span>Multilevel</span> <i class="fa fa-angle-left pull-right"></i></a>
-                <ul class="treeview-menu">
-                    <li><a href="#">Link in level 2</a></li>
-                    <li><a href="#">Link in level 2</a></li>
-                </ul>
-            </li>
+            <li><a href="/employees"><span>Medewerkers</span></a></li>
+            <li><a href="/add"><span>Medewerker Toevoegen</span></a></li>
         </ul><!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
