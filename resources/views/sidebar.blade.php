709720c677ccf074b9eb4f474d2adee2c6632e23
Omar <info@zoundri.nl>
2015-10-28 13:16:24 +0100

    added gaben

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -7,7 +7,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="{{ asset("/docs/dist/img/user2-160x160.jpg") }}" class="img-circle" alt="User Image" />
+                <img src="{{ asset("/docs/dist/img/gaben.jpg") }}" class="img-circle" alt="User Image" />
             </div>
             <div class="pull-left info">
                 <p>Naam</p>
