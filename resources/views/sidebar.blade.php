932ec2aa4bd6b9c95bea75565d892960f5f38d5b
Omar <info@zoundri.nl>
2015-10-28 13:16:48 +0100

    no message

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -10,7 +10,7 @@
                 <img src="{{ asset("/docs/dist/img/gaben.jpg") }}" class="img-circle" alt="User Image" />
             </div>
             <div class="pull-left info">
-                <p>Naam</p>
+                <p>Gaben</p>
                 <!-- Status -->
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
